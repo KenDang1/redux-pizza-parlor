284dc471066de13096e02424ea508fb344d76b15
Ken Dang <k3ndang@gmail.com>
2022-01-20 14:00:52 -0600

    pizzaItem

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import PizzaListItem from '../PizzaListItem/PizzaListItem.jsx'
 
 const PizzaList = () => {
     const pizzaList = useSelector(store => store.pizzaReducer);
 
     return(
         <div>
-            {pizzaList.map((pizza, i) => {
-                <div key={i}>{pizza.name}</div>
-            })}
+            {pizzaList.map((pizza, i) => 
+                <PizzaListItem key={i} pizza={pizza}/>
+            )}
         </div>
     )
 }
