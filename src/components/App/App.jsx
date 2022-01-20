9ddfe7b70608fbf672761bff2c60c49620ef84b9
Desiree Quade <nekodez@gmail.com>
2022-01-20 10:53:00 -0600

    working on get

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // setup useEffect to call fetchPizza function
+  useEffect(() => {
+    fetchPizza();
+  }, []);
+
+  const fetchPizza = () => {
+    console.log('in fetchPizza');
+
+    // send request to server side
+    axios.get('/api/pizza')
+      .then((res) => {
+        // tell client of success
+        console.log('pizza from database:', res.data);
+
+        // set 
+      })
+      .catch((err) => {
+        // tell client of failure
+        console.error('axios GET ERROR!', err);
+      })
+  }
 
   return (
     <div className='App'>
